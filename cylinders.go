c122f8752d0d2972df3cf05ca2d470f661b53556
Tim Butram <tbutram@worldsproject.org>
2015-02-12 23:55:36 -0500

    Added Cylinder module.

---
+++ cylinders.go
@@ -0,0 +1,27 @@
+package noiselib
+
+import "math"
+
+type Cylinders struct {
+	Frequency float64
+}
+
+func (c Cylinders) GetSourceModule(index int) Module {
+	return nil
+}
+
+func (c Cylinders) SetSourceModule(index int, sourceModule Module) {
+	return
+}
+
+func (c Cylinders) GetValue(x, y, z float64) float64 {
+	x *= c.Frequency
+	y *= c.Frequency
+
+	distFromCenter := math.Sqrt(x*x + z*z)
+	distFromSmallerSphere := distFromCenter - math.Floor(distFromCenter)
+	distFromLargerSphere := 1.0 - distFromSmallerSphere
+	nearestDist := math.Min(distFromSmallerSphere, distFromLargerSphere)
+
+	return 1.0 - (nearestDist * 4.0)
+}
