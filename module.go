5439f590edd8670899b3e9118c8247a0ac64a7f6
Tim Butram <tbutram@worldsproject.org>
2015-02-18 09:34:07 -0500

    Added a clamp function for float64

--- module.go
+++ module.go
@@ -2,25 +2,35 @@ package noiselib
 
 type Module interface {
 
-  //Returns a source module connected to thise noise module.
-  GetSourceModule(index int) Module
+	//Returns a source module connected to thise noise module.
+	GetSourceModule(index int) Module
 
-  //Generates an output value given the coordinates of the specified input
-  // value. Before an applicaion can call this method, it must first connect
-  // all required source modules via SetSourceModule(). If these source modules
-  // are not connected to this module, this method raises an error.
-  GetValue(x, y, z float64) float64
+	//Generates an output value given the coordinates of the specified input
+	// value. Before an applicaion can call this method, it must first connect
+	// all required source modules via SetSourceModule(). If these source modules
+	// are not connected to this module, this method raises an error.
+	GetValue(x, y, z float64) float64
 
-  //Sets a source module in the given index.
-  SetSourceModule(index int, sourceModule Module)
+	//Sets a source module in the given index.
+	SetSourceModule(index int, sourceModule Module)
 }
 
-func ClampValue(value, lowerBound, upperBound int) (int) {
-  if value < lowerBound {
-    return lowerBound
-  } else if value > upperBound {
-    return value
-  } else {
-    return value
-  }
+func ClampValue(value, lowerBound, upperBound int) int {
+	if value < lowerBound {
+		return lowerBound
+	} else if value > upperBound {
+		return value
+	} else {
+		return value
+	}
+}
+
+func ClampValueFloat(value, lowerBound, upperBound float64) float64 {
+	if value < lowerBound {
+		return lowerBound
+	} else if value > upperBound {
+		return upperBound
+	} else {
+		return value
+	}
 }
