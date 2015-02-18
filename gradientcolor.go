3869fb589e85b748f66c3532f6cf86ab8d53b254
Tim Butram <tbutram@worldsproject.org>
2015-02-18 00:57:15 -0500

    Added a Gradient type struct.

---
+++ gradientcolor.go
@@ -0,0 +1,42 @@
+package noiselib
+
+import "image/color"
+
+type GradientColor struct {
+	GradientPoints map[float64]color.RGBA
+}
+
+func (g GradientColor) AddGradientPoint(position float64, color color.RGBA) {
+	g.GradientPoints[position] = color
+}
+
+func (g GradientColor) GetColor(position float64) color.RGBA {
+	keys := []float64{}
+	for k, _ := range g.GradientPoints {
+		keys = append(keys, k)
+	}
+
+	indexPos := 0
+
+	for i, k := range keys {
+		if position < k {
+			indexPos = i
+			break
+		}
+	}
+
+	index0 := ClampValue(indexPos-1, 0, len(g.GradientPoints))
+	index1 := ClampValue(indexPos, 0, len(g.GradientPoints))
+
+	if index0 == index1 {
+		return g.GradientPoints[keys[index1]]
+	}
+
+	input0 := keys[index0]
+	input1 := keys[index1]
+	alpha := (float64(position) - input0) / (input1 - input0)
+
+	color0 := g.GradientPoints[keys[index0]]
+	color1 := g.GradientPoints[keys[index0]]
+	return LinearInterpColor(color0, color1, alpha)
+}
